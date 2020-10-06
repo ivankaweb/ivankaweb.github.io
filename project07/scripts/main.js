36cb7b19fca6ccd08c824afefe70c1861be0db0e
ivankaweb <70915277+ivankaweb@users.noreply.github.com>
2020-10-06 17:44:21 +0200

    new project07

---
+++ project07/scripts/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $('.js-btn-color a').click(function(){
+        var buttonActive = $(this);
+        var myAttr = buttonActive.attr('filter');
+ 
+        if(myAttr == buttonActive){
+          $('.image div').show(500);
+        }else{
+          $('.image div').hide(500);
+          $('div[filter="'+ myAttr +'"]').show(500);
+        }
+        $('.js-btn-color a').removeClass('btn-active');
+        buttonActive.addClass('btn-active');
+     });
+     
+    $('.js-btn-small a').click(function(){
+        var buttonActive = $(this);
+        var myAttr = buttonActive.attr('filter');
+ 
+        if(myAttr == buttonActive){
+          $('.image div').show(900);
+        }else{
+          $('.image div').hide(500);
+          $('div[filter="'+ myAttr +'"]').show(900);
+        }
+        
+        $('.js-btn-small a').removeClass('btn-active-small');
+        buttonActive.addClass('btn-active-small');
+     });
+
+
+
+    });
