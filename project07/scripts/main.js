a039e819f7749155014a54de8a2aff5104d2212a
ivankaweb <70915277+ivankaweb@users.noreply.github.com>
2020-10-07 15:20:17 +0200

    js add class mobile

--- project07/scripts/main.js
+++ project07/scripts/main.js
@@ -1,38 +1,54 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.js-btn-color a').click(function(){
-        var buttonActive = $(this);
-        var myAttr = buttonActive.attr('filter');
- 
-        if(myAttr == buttonActive){
-          $('.image div').show(500);
-        }else{
-          $('.image div').hide(500);
-          $('div[filter="'+ myAttr +'"]').show(500);
-        }
-        $('.js-btn-color a').removeClass('btn-active');
-        buttonActive.addClass('btn-active');
-     });
-     
-    $('.js-btn-small a').click(function(){
-        var buttonActive = $(this);
-        var myAttr = buttonActive.attr('filter');
- 
-        if(myAttr == buttonActive){
-          $('.image div').show(900);
-        }else{
-          $('.image div').hide(500);
-          $('div[filter="'+ myAttr +'"]').show(900);
-        }
-        
-        $('.js-btn-small a').removeClass('btn-active-small');
-        buttonActive.addClass('btn-active-small');
-     });
+  $('.js-btn-color a').click(function () {
+    var buttonActive = $(this);
+    var myAttr = buttonActive.attr('filter');
 
-     $('.dropbtn').click(function(){
-      $('div .dropdown-content').toggle( 'slow', function(){
-        $('a .dropdown-content').css({'display' : 'inline-block'});
+    if (myAttr == buttonActive) {
+      $('.image div').show(500);
+    } else {
+      $('.image div').hide(500);
+      $('div[filter="' + myAttr + '"]').show(500);
+    }
+    $('.js-btn-color a').removeClass('btn-active');
+    buttonActive.addClass('btn-active');
+  });
 
-      });
-     });
+  $('.js-btn-small a').click(function () {
+    var buttonActive = $(this);
+    var myAttr = buttonActive.attr('filter');
+
+    if (myAttr == buttonActive) {
+      $('.image div').show(900);
+    } else {
+      $('.image div').hide(500);
+      $('div[filter="' + myAttr + '"]').show(900);
+    }
+
+    $('.js-btn-small a').removeClass('btn-active-small');
+    buttonActive.addClass('btn-active-small');
+  });
+
+  $('.dropbtn').click(function () {
+    $('div .dropdown-content').toggle('slow', function () {
+      $('a .dropdown-content').css({ 'display': 'inline-block' });
+
     });
+  });
+
+
+  $('.js-btn-nav').click(function () {
+    $('ul.menu').toggle(900);
+    $('nav.mobile-nav').css({
+      'display': 'block'
+    })
+
+    if ($('i', this).hasClass('fa-align-justify')) {
+      $(this).html('<i class="fas fa-align-justify"></i>')
+
+    } else {
+      $(this).html('<i class="fas fa-align-center"></i>')
+    }
+  });
+
+});
